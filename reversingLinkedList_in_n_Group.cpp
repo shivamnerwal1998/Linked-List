be6d6727a7451d08bb2d1a3e998cf856f38da9ad
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-11-10 01:10:00 +0530

    reversing Linked List in Group
    if input list is:
    1-> 2 -> 3 -> 4 -> 5-> 6
    k is the number of group
    k = 2 
    then output is:
    2->1->4->3->6->5

--- reversingLinkedList_in_n_Group.cpp
+++ reversingLinkedList_in_n_Group.cpp
@@ -1,20 +1,45 @@
 #include<iostream>
-using manespace std ;
+using namespace std ;
 
 struct node
 {
     int info ;
-    node *next ;
+    struct node *next ;
 
 };
-void insertFront(node **head ; int data)
+
+void insertFront(node **head , int data)
 {
-    node ptr ;
+    node *ptr;
     ptr =(node *)malloc(sizeof(node));
     ptr->info = data ;
     ptr->next = (*head);
     (*head) = ptr ;
 }
+traverse(node *head)
+{
+    for( ; head != NULL ;head = head-> next )
+        cout << head->info << "->" ;
+}
+node* reverseList(node *headREF, int k)
+{
+    node *current  = headREF;
+    node *prev = NULL ;
+    node *next = NULL ;
+    int count = 0 ;
+    while(current != NULL && count<k )
+    {
+        count++ ;
+        next = current->next ;
+        current -> next = prev ;
+        prev = current ;
+        current = next ;
+    }
+    if(next != NULL)
+        headREF->next = reverseList(next , k);
+        return prev ;
+}
+
 int main()
 {
     node *head ;
@@ -23,7 +48,13 @@ int main()
     insertFront(&head,2);
     insertFront(&head,3);
     insertFront(&head,4);
+    cout << "\n List is  : \n" ;
     traverse(head);
-
+    cout << "\n Reversing the in group  \n" ;
+    cout << "Enter number of groups \n" ;
+    int k ;
+    cin >> k ;
+    head = reverseList(head,k) ;
+    traverse(head);
     return 0 ;
 }
