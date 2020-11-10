9ea99d60553d09c2a31bf7e43e620e508bb81a57
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2020-11-10 11:37:28 +0530

    Detect loop in a linked list
    this is the function (floyd's algorithm) 
    to detect loop in a linked list

---
+++ floyd's_algo(loop detection in linked list ).cpp
@@ -0,0 +1,18 @@
+
+bool detectLoop(Node* head)
+{
+     Node *slow = head ;
+     Node *fast = head ;
+     int count = 0 ;
+    while( fast != NULL && fast->next != NULL )
+    {
+        slow = slow-> next ;
+        fast = (fast->next)->next ;
+        if(slow == fast )
+            return true ;
+
+    }
+    return false ;
+
+}
+
